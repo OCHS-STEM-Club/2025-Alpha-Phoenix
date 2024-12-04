af890cc8e00b8df6a24a115b42fdda4aeca2eb25
princerodrick59 <116687818+princerodrick59@users.noreply.github.com>
2024-12-04 08:12:30 -0600

    Added path planner auto chooser and Added constants file

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,68 +6,74 @@ package frc.robot;
 
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+import com.pathplanner.lib.auto.AutoBuilder;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.DriveConstants;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class RobotContainer {
-  private double MaxSpeed = TunerConstants.kSpeedAt12VoltsMps; // kSpeedAt12VoltsMps desired top speed
-  private double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
+
+  private final SendableChooser<Command> autoChooser;
 
   /* Setting up bindings for necessary control of the swerve drive platform */
-  private final CommandXboxController joystick = new CommandXboxController(0); // My joystick
-  private final CommandSwerveDrivetrain drivetrain = TunerConstants.DriveTrain; // My drivetrain
+  private final CommandXboxController m_driverController = new CommandXboxController(DriveConstants.kDriverControllerPort); 
+
+  private final CommandSwerveDrivetrain drivetrain = TunerConstants.DriveTrain;
 
   private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-      .withDeadband(MaxSpeed * 0.2).withRotationalDeadband(MaxAngularRate * 0.2) // Add a 10% deadband
-      .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // I want field-centric
-                                                               // driving in open loop
+      .withDeadband(DriveConstants.MAX_SPEED * DriveConstants.TRANSLATION_DEADBAND)
+      .withRotationalDeadband(DriveConstants.MAX_ANGULAR_RATE * DriveConstants.ROTATION_DEADBAND) 
+      .withDriveRequestType(DriveRequestType.OpenLoopVoltage); 
                                                                
   private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
   private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
-  private final Telemetry logger = new Telemetry(MaxSpeed);
+  private final Telemetry logger = new Telemetry(DriveConstants.MAX_SPEED);
 
   private void configureBindings() {
     
     
     drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
-        
-    drivetrain.applyRequest(() -> 
-
-        drive.withVelocityX(joystick.getLeftY() * MaxSpeed)
-            .withVelocityY(joystick.getLeftX() * MaxSpeed) 
-            .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
-        
+        drivetrain.applyRequest(() -> 
+            drive.withVelocityX(m_driverController.getLeftY() * DriveConstants.MAX_SPEED)
+            .withVelocityY(m_driverController.getLeftX() * DriveConstants.MAX_SPEED) 
+            .withRotationalRate(-m_driverController.getRightX() * DriveConstants.MAX_ANGULAR_RATE) 
             ));
 
-    joystick.x().whileTrue(drivetrain.applyRequest(() -> brake));
+      m_driverController.x().whileTrue(drivetrain.applyRequest(() -> brake));
     
-    joystick.b().whileTrue(drivetrain
-        .applyRequest(() -> point.withModuleDirection(
-          new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
+      m_driverController.b().whileTrue(drivetrain
+          .applyRequest(() -> point.withModuleDirection(
+          new Rotation2d(-m_driverController.getLeftY(), -m_driverController.getLeftX()))));
 
     // reset the field-centric heading on left bumper press
-    joystick.a().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
+      m_driverController.a().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
 
-    if (Utils.isSimulation()) {
+      if (Utils.isSimulation()) {
       drivetrain.seedFieldRelative(new Pose2d(new Translation2d(), Rotation2d.fromDegrees(90)));
-    }
-    drivetrain.registerTelemetry(logger::telemeterize);
+      }
+      drivetrain.registerTelemetry(logger::telemeterize);
   }
 
   public RobotContainer() {
+    autoChooser = AutoBuilder.buildAutoChooser();
+
     configureBindings();
+
+    SmartDashboard.putData("Auto Chooser", autoChooser);
   }
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    return autoChooser.getSelected();
   }
 }
